948b3436a41f4b3b6bfaea787a3764e3e0e22565
sergioGN04 <sergio.gonzalez.noria.alu@iesfernandoaguilar.es>
2025-03-30 21:28:45 +0200

    Actualización - Frontend Login y Register

---
+++ sources/frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderNoAutorizadoComponent } from '../../components/shared/header-no-autorizado/header-no-autorizado.component';
+import { FooterComponent } from '../../components/shared/footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [HeaderNoAutorizadoComponent,FooterComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
