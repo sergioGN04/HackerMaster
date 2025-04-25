d6ffeaa962b87101a0fd7fd9d3a62cd8bcaac11e
dryke48 <dryke48@gmail.com>
2025-04-25 20:43:06 +0200

    Actualización Implementacion Seguridad Backend (XSS, CORS, Logs con morgan)

--- sources/backend/app.js
+++ sources/backend/app.js
@@ -3,6 +3,11 @@ const body_parser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+// Archivo para guardar los logs
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
 
 dotenv.config();
 
@@ -13,13 +18,42 @@ const app = express();
 app.use(express.json())
 app.use(body_parser.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Middleware para tener un registro de las peticiones
+app.use(morgan('common', { stream: accessLogStream }));
+
+// Middleware para proteger la aplicación contra ataques de tipo XSS
+app.use(helmet());
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'", 'https:'],
+            imgSrc: ["'self'", 'data:'],
+        },
+    })
+);
+
+// Para proteger contra ataques XSS en HTTPS
+//app.use(
+//    helmet.hsts({
+//        maxAge: 63072000, // 2 años en segundos
+//        includeSubDomains: true,
+//        preload: true,
+//    })
+//);
 
 const sequelize = require('./config/database');
 
 sequelize.authenticate()
-    .then(() => { console.log("Se ha conectado a la base de datos correctamente");})
-    .catch((error) => { console.error("Error - No se ha podido conectar a la base de datos",error);});
+    .then(() => { console.log("Se ha conectado a la base de datos correctamente"); })
+    .catch((error) => { console.error("Error - No se ha podido conectar a la base de datos", error); });
 
 sequelize.sync({ alter: true })
     .then(() => console.log('Se han creado las tablas correctamente'))
